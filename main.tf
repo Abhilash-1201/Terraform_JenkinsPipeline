424149325a421270f17586066f28f332c930264d
Abhilash-1201 <83076098+Abhilash-1201@users.noreply.github.com>
2022-06-07 16:50:46 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+resource "aws_instance" "terraform_wapp" {
+    ami = "ami-005de95e8ff495156"
+    instance_type = "t2.micro"
+    key_name               = "ELK_ubuntu"
+    count         = 1
+    associate_public_ip_address = true
+    user_data = "${file("install_apache.sh")}"
+
+    tags = {
+      "Name"      = "Apache server"
+      "Terraform" = "true"
+    }
+}
